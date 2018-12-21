91a65a3ea21f98766a0784fce0726086f7b921fa
mdashrafbd <mdashrafbd@gmail.com>
2018-12-21 10:31:04 +0600

    First Commit

---
+++ day1/FirstProgram/FirstProgram/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            a = 10;
+            b = 20;
+            c = a + b;
+            Console.WriteLine(c);
+            Console.ReadLine();
+
+        }
+    }
+}
